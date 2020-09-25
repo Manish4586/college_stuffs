d7a21f3e11b213108beec83d9547e2e48463f971
Manish4586 <manish.n.manish45@gmail.com>
2020-09-25 11:13:50 +0530

    cart: add some visual touch and refactor with price
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- shopping_cart/shopping_cart.c
+++ shopping_cart/shopping_cart.c
@@ -3,7 +3,7 @@
 int main()
 {
     char item[500][50];
-    int size[1000];
+    int price[1000];
     int n, i, j, tmp;
 
 /*    printf("Enter Product Name :");
@@ -16,26 +16,26 @@ int main()
     printf("%d Enter Product Name :",i);
     scanf("%s",&item[i]);
     printf("%d Enter Product Price :",i);
-    scanf("%d",&size[i]);
+    scanf("%d",&price[i]);
     }
 
     for(i=0; i<n; i++)
     {
     for(j=i+1; j<n; j++)
     {
-    if(size[j] <size[i])
+    if(price[j] < price[i])
     {
-    tmp = size[i];
-    size[i] = size[j];
-    size[j] = tmp;
+    tmp = price[i];
+    price[i] = price[j];
+    price[j] = tmp;
     }
    }
   }
-    printf("\nItems In Your Cart :\n");
+    printf("%-9s%15s\n", "Item", "Price");
     for(i=0; i<n; i++)
     {
-    printf("Product:%s ", item[i]);
-    printf("Price:%d\n", size[i]);
+    printf("%-10s %10.2d\n", item[i], price[i]);
     }
         //printf("\n\n");
 }
+
